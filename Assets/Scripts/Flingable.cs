1f708401deba058d2a737f52a2c32e44bd4594a7
Ehud Rosenberg <ehud@funtomic.com>
2016-11-21 22:20:44 +0200

    Basic fling + virus art

---
+++ Assets/Scripts/Flingable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flingable : MonoBehaviour {
+	bool didInitiatedFling;
+
+	Vector3 flingStartPosition;
+	SpriteRenderer coneSprite;
+	Vector2 aimDirection;
+	float aimAngle;
+
+	// Use this for initialization
+	void Start () {
+		coneSprite = GameObject.Find ("cone").GetComponent<SpriteRenderer> ();
+		coneSprite.enabled = false;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		if (Input.GetMouseButtonDown (0) && !didInitiatedFling) {
+			// begin aim
+			didInitiatedFling = true;
+			flingStartPosition = mousePos;
+			coneSprite.enabled = true;
+		//	GameObject.Find ("marker").transform.position = new Vector3 (mousePos.x, mousePos.y, 0);
+		}
+		if (Input.GetMouseButtonUp (0) && didInitiatedFling) {
+			// launch
+			didInitiatedFling = false;
+			coneSprite.enabled = false;
+			GetComponent<Virus> ().Launch (aimDirection.normalized, aimAngle);
+
+		}
+
+		if (didInitiatedFling) {			
+			// update aim arrow
+			aimDirection = transform.position - mousePos;
+			aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+			if(aimAngle < 0) aimAngle += 360;
+
+			coneSprite.transform.localEulerAngles = new Vector3 (0, 0, aimAngle);
+		}
+			
+
+	}
+}
---
+++ Assets/Virus.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Virus : MonoBehaviour {
+	Rigidbody2D body;
+	Animator animator;
+	Transform spriteObject;
+	bool isFlying;
+
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+		body.drag = 3f;
+		animator = GetComponentInChildren<Animator> ();
+		spriteObject = transform.Find ("Sprite");
+		SetIdle ();
+
+	}
+
+
+	public void Launch(Vector2 direction, float angle)
+	{
+		body.velocity = (direction * 30);
+		//transform.localRotation
+		animator.Play("Virus Flying");
+		spriteObject.localScale = Vector3.one;
+		//spriteObject.localEulerAngles = new Vector3 (0, 0, -angle);
+		DOTween.Kill (spriteObject);
+		spriteObject.DOScale (new Vector3 (1.7f, 0.2f, 0), 0.2f).SetEase(Ease.InOutSine).SetLoops (-1, LoopType.Yoyo);
+
+		isFlying = true;
+	}
+
+	void Update()
+	{
+		if (body.velocity.magnitude < 5 && isFlying)
+			SetIdle ();
+	}
+		
+	void SetIdle()
+	{
+		isFlying = false;
+		animator.Play ("Virus Idle");
+		spriteObject.localScale = Vector3.one;
+		DOTween.Kill (spriteObject);
+		spriteObject.DOScale (new Vector3 (1.3f, 0.7f, 0), 0.25f).SetEase(Ease.InOutSine).SetLoops (-1, LoopType.Yoyo);
+	}
+}
