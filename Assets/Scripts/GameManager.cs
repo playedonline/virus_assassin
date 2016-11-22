af434d533307d3f9c0a69f1f848c89d18d6d2280
Tom Amel <ameltom@gmail.com>
2016-11-22 11:30:39 +0200

    new mooving mechanizem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,36 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public const float SCREEN_WIDTH = 10.8f;
+    public const float SCREEN_HEIGHT = 19.2f;
+    public const float SCREENS_WIDTH = 1;
+    public const float SCREENS_HEIGHT = 1;
+
     public int regularFiguresAmount = 0;
 
-    private Object m_wandererPrefab;
+    private Object m_hostFigurePrefab;
+
+    public Vector3 TopLeft {get {
+        return new Vector3(-SCREEN_WIDTH * (SCREENS_WIDTH - 0.5f), SCREEN_HEIGHT * (SCREENS_HEIGHT - 0.5f));
+    }}
+
+    public Vector3 BottomRight {get {
+        return new Vector3(SCREEN_WIDTH * (SCREENS_WIDTH - 0.5f), -SCREEN_HEIGHT * (SCREENS_HEIGHT - 0.5f));
+    }}
 
     public void Init(){
+        Vector3 topLeft = TopLeft;
+        Vector3 bottomRight = BottomRight;
+
         for(int i = 0 ; i < regularFiguresAmount ; i++){
-            GameObject wanderer = Instantiate(m_wandererPrefab) as GameObject;
-            wanderer.transform.localPosition = new Vector3(Random.Range(-5.5f, 5.5f), Random.Range(-9.8f, 9.8f), 0);
+            GameObject hostFigure = Instantiate(m_hostFigurePrefab) as GameObject;
+            hostFigure.transform.localPosition = new Vector3(Random.Range(topLeft.x, bottomRight.x), Random.Range(bottomRight.y, topLeft.y), 0);
+            hostFigure.GetComponent<HostFigure>().Init(topLeft, bottomRight);
         }
     }
 
     void Awake(){
-        m_wandererPrefab = Resources.Load("Soldier");
+        m_hostFigurePrefab = Resources.Load("SoldierOur");
 		Application.targetFrameRate = 60;
         Init();
     }
--- Assets/Scripts/HostFigure.cs
+++ Assets/Scripts/HostFigure.cs
@@ -1,18 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnitySteer2D.Behaviors;
 using DG.Tweening;
 public class HostFigure : MonoBehaviour {
+    private const float PATH_POINT_DIFF = 3;
+
     public string baseAssetsName;
     public float maxSpeed = 3;
     public float timeToLive = 30f;
+	public HostFigureType hostType;
 
     private bool m_isInfected = false;
     private float m_infectedTime = 0;
-    private AutonomousVehicle2D m_autonomousVehicle2d;
+    private PositionLimit m_posLimit;
     private SpriteRenderer m_spriteRenderer;
 	private Animator m_animator;
-	public HostFigureType hostType;
-	
+    private List<Vector3> m_pathPoints;
+    private int pointIndex = 0;
+    private Tweener currentMoveTween;
+
+
+    public void Init(Vector3 topLeft, Vector3 bottomRight){
+        m_pathPoints = new List<Vector3>();
+        m_pathPoints.Add(transform.localPosition);
+        Vector3 lastPos;
+        for(int i = 0 ; i < Random.Range(4,10) ; i++){
+            lastPos = m_pathPoints[m_pathPoints.Count - 1];
+            m_pathPoints.Add(new Vector2(
+                    Random.Range(Mathf.Max(topLeft.x, lastPos.x - PATH_POINT_DIFF), Mathf.Min(bottomRight.x, lastPos.x + PATH_POINT_DIFF)),
+                    Random.Range(Mathf.Max(bottomRight.y, lastPos.y - PATH_POINT_DIFF), Mathf.Min(topLeft.y, lastPos.y + PATH_POINT_DIFF))
+            ));
+        }
+
+        MoveToNextPoint();
+    }
+
+    private void MoveToNextPoint(){
+        pointIndex += 1;
+        Vector3 nextPoint = m_pathPoints[pointIndex];
+
+        Debug.Log("MoveToNextPoint");
+        m_spriteRenderer.transform.localScale = new Vector3 (nextPoint.x - transform.localPosition.x < 0 ? 1 : -1, 1, 1);
+
+        currentMoveTween = transform.DOLocalMove(nextPoint, 2).SetEase(Ease.Linear).OnComplete(() => {
+            if(m_pathPoints.Count - 1 <= pointIndex){
+                m_pathPoints.Reverse();
+                pointIndex = 0;
+            }
+
+            MoveToNextPoint();
+        });
+    }
 
     public void Infect() {
         if(m_isInfected)
@@ -35,9 +72,6 @@ public class HostFigure : MonoBehaviour {
     }
 
     void Awake() {
-        m_autonomousVehicle2d = GetComponent<AutonomousVehicle2D>();
-        m_autonomousVehicle2d.MaxSpeed = maxSpeed;
-
         m_spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 		m_animator = GetComponentInChildren<Animator> ();
 		hostType = HostFigureType.Soldier;
@@ -55,17 +89,8 @@ public class HostFigure : MonoBehaviour {
 
     void Update() {
         m_spriteRenderer.transform.rotation = Quaternion.identity;
-		if (m_autonomousVehicle2d.Velocity.x > 0)
-			m_spriteRenderer.transform.localScale = new Vector3 (-1, 1, 1);
-		else
-			m_spriteRenderer.transform.localScale = new Vector3 (1, 1, 1);
-
-
-        if (m_isInfected) {
-        //Debug.Log("TimeLeftToLive: " + TimeLeftToLive());
-            if(TimeLeftToLive() < 0)
-                die();
-        }
+        if (m_isInfected && TimeLeftToLive() < 0)
+            die();
     }
 	public void OnHit()
 	{
