0fc0d2e9653f290da8148a5d6b275f83ccf5359c
Tom Amel <ameltom@gmail.com>
2016-11-23 14:43:09 +0200

    arrow as prefab

--- Assets/OffscreenPointer.cs
+++ Assets/OffscreenPointer.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class OffscreenPointer : MonoBehaviour {
 	public Transform target;    
-	public Transform origin; 
+	public Transform origin;
+    public Transform arrow;
 
 	// Use this for initialization
 	void Start () {
-		transform.DOScale (Vector3.one * 0.2f, 1).SetEase (Ease.OutBounce).SetLoops (-1, LoopType.Yoyo).SetRelative(true);
+        arrow.transform.DOScale (Vector3.one * 0.2f, 1).SetEase (Ease.OutBounce).SetLoops (-1, LoopType.Yoyo).SetRelative(true);
 	}
 
 	public void Init(Transform target, Transform origin)
@@ -35,13 +36,13 @@ public class OffscreenPointer : MonoBehaviour {
 		Vector3 targetViewportPosition = Camera.main.WorldToViewportPoint (target.transform.position);
 		bool inViewport = (targetViewportPosition.x > 0 && targetViewportPosition.x < 1 && targetViewportPosition.y > 0 && targetViewportPosition.y < 1);
 		//gameObject.SetActive (!inViewport);
-		transform.localEulerAngles = new Vector3 (0, 0, pointerAngle); 
+        arrow.transform.localEulerAngles = new Vector3 (0, 0, pointerAngle);
 			
 		if (!inViewport) {
 			targetViewportPosition.x = Mathf.Clamp (targetViewportPosition.x, 0, 1);
 			targetViewportPosition.y = Mathf.Clamp (targetViewportPosition.y, 0, 1);
 			transform.position = Camera.main.ViewportToWorldPoint (targetViewportPosition);
 		} else
-			transform.position = target.position;
+			transform.position = target.position.;
 	}
 }
