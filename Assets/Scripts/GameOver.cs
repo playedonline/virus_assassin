c546311e584c4b9bc640d18bbbd75c7f67f7431c
Ehud Rosenberg <ehud@funtomic.com>
2016-11-23 14:46:14 +0200

    fewfwe

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -9,22 +9,34 @@ public class GameOver : MonoBehaviour {
     private bool allowRestart = false;
 
     void Awake() {
-        scoreText.text = string.Format("SCORE: {0}", GameManager.Instance.score);
+        //
 
-		DOVirtual.DelayedCall(0.5f, () => {
-            allowRestart = true;
-            DOTween.Sequence().Append(
-                    tapToStart.DOFade(1, 0.2f)
-            ).Append(
-                    tapToStart.DOFade(1, 0.2f)
-            ).Append(
-                    tapToStart.DOFade(0, 0.2f)
-            ).SetLoops(-1, LoopType.Restart);
-        });
+
     }
 
     void Update(){
         if(Input.GetMouseButtonDown(0) && allowRestart)
             UnityEngine.SceneManagement.SceneManager.LoadScene("main");
     }
+
+	public void Show(bool success)
+	{
+		gameObject.SetActive (true);
+
+		if (success)
+			scoreText.text = string.Format("임무 완수!");
+		else
+			scoreText.text = string.Format("실패!");
+		
+		DOVirtual.DelayedCall(0.5f, () => {
+			allowRestart = true;
+			DOTween.Sequence().Append(
+				tapToStart.DOFade(1, 0.2f)
+			).Append(
+				tapToStart.DOFade(1, 0.2f)
+			).Append(
+				tapToStart.DOFade(0, 0.2f)
+			).SetLoops(-1, LoopType.Restart);
+		});
+	}
 }
